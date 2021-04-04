c6036958bc8e56ace21e724c0f2cd1be8df034e6
GUDUSATY2001 <78610090+GUDUSATY2001@users.noreply.github.com>
2021-04-04 15:39:47 +0530

    Add files via upload

---
+++ week3/Satyam Sabat Assignment 3/q5-4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+int reverse(int num);
+int isPalindrome(int num);
+
+
+
+int main()
+{
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    
+    if(isPalindrome(num) == 1)
+    {
+        printf("%d is palindrome number.\n", num);
+    }
+    else
+    {
+        printf("%d is NOT palindrome number.\n", num);
+    }
+    
+    return 0;
+}
+
+int isPalindrome(int num)
+{
+    if(num == reverse(num))
+    {
+        return 1;
+    }
+    
+    return 0;
+}
+
+int reverse(int num)
+{
+    
+    int digit = (int)log10(num);
+    
+    if(num == 0)
+        return 0;
+
+    return ((num%10 * pow(10, digit)) + reverse(num/10));
+}
